69e3e87b5a22747f4632bc7c75e92d626beaedee
bii_12 <18753896666@163.com>
2025-05-14 15:58:18 +0800

    finish all, prepare to PR

--- tests/test_llms.py
+++ tests/test_llms.py
@@ -8,7 +8,7 @@ from panda_guard.llms.oai import (
     OpenAiChatLLM,
 )
 from panda_guard.llms.hf import HuggingFaceLLMConfig, HuggingFaceLLM
-from panda_guard.llms.vllm import VLLMLLMConfig, VLLMLLM
+from panda_guard.llms.vllm_llm import VLLMLLMConfig, VLLMLLM
 from panda_guard.llms.gemini import GeminiLLMConfig, GeminiLLM
 from panda_guard.llms.claude import ClaudeLLM, ClaudeLLMConfig
 from panda_guard.llms.sglang_llm import SGLangLLM, SGLangLLMConifg
@@ -33,9 +33,15 @@ class TestLLMs:
         )
 
     @pytest.mark.api
+    @pytest.mark.skipif(
+        not os.getenv(
+            "GEMINI_API_KEY"
+        ),  # Condition: True if GEMINI_API_KEY is not set or is empty
+        reason="GEMINI_API_KEY environment variable not set or is empty",
+    )
     def test_gemini_gen(self, input_prompt):
         config = GeminiLLMConfig(
-            model_name="gemini-1.5-pro", api_key=os.getenv("GOOGLE_API_KEY")
+            model_name="gemini-1.5-pro", api_key=os.getenv("GEMINI_API_KEY")
         )
 
         llm = GeminiLLM(config)
@@ -44,6 +50,12 @@ class TestLLMs:
         assert check_response(response_msg) is True
 
     @pytest.mark.api
+    @pytest.mark.skipif(
+        not os.getenv(
+            "ANTHROPIC_API_KEY"
+        ),  # Condition: True if ANTHROPIC_API_KEY is not set or is empty
+        reason="ANTHROPIC_API_KEY environment variable not set or is empty",
+    )
     def test_claude_gen(self, input_prompt):
         config = ClaudeLLMConfig(
             model_name="claude-3-opus-20240229", api_key=os.getenv("ANTHROPIC_API_KEY")
@@ -55,10 +67,17 @@ class TestLLMs:
         assert check_response(response_msg) is True
 
     @pytest.mark.api
+    @pytest.mark.skipif(
+        not os.getenv(
+            "OPENAI_API_KEY"
+        ),  # Condition: True if OPENAI_API_KEY is not set or is empty
+        reason="OPENAI_API_KEY environment variable not set or is empty",
+    )
     def test_openaillm_gen(self, input_prompt):
         config = OpenAiLLMConfig(
+            model_name="gpt-4.1",
             base_url="https://api.openai.com/v1",
-            api_key="sk-YOUR_ACTUAL_API_KEY_HERE",
+            api_key=os.getenv("OPENAI_API_KEY"),
         )
         llm = OpenAiLLM(config=config)
         messages = [{"role": "user", "content": input_prompt}]
@@ -66,17 +85,24 @@ class TestLLMs:
         assert check_response(response_msg) is True
 
     @pytest.mark.api
+    @pytest.mark.skipif(
+        not os.getenv(
+            "OPENAI_API_KEY"
+        ),  # Condition: True if OPENAI_API_KEY is not set or is empty
+        reason="OPENAI_API_KEY environment variable not set or is empty",
+    )
     def test_openaichatllm_gen(self, input_prompt):
         config = OpenAiChatLLMConfig(
+            model_name="gpt-4.1",
             base_url="https://api.openai.com/v1",
-            api_key="sk-YOUR_ACTUAL_API_KEY_HERE",
+            api_key=os.getenv("OPENAI_API_KEY"),
         )
         llm = OpenAiChatLLM(config=config)
         messages = [{"role": "user", "content": input_prompt}]
         response_msg = llm.generate(messages=messages, config=self.llm_gen_config())
         assert check_response(response_msg) is True
 
-    @pytest.mark.skip(reason="skip hf llm")
+    # @pytest.mark.skip(reason="skip hf llm")
     def test_hfllm_gen(self, input_prompt):
         config = HuggingFaceLLMConfig(
             model_name="Qwen/Qwen3-0.6B",
@@ -87,7 +113,7 @@ class TestLLMs:
         response_msg = llm.generate(messages=messages, config=self.llm_gen_config())
         assert check_response(response_msg) is True
 
-    @pytest.mark.skip(reason="skip vllm llm")
+    # @pytest.mark.skip(reason="skip vllm llm")
     def test_vllmllm_gen(self, input_prompt):
         config = VLLMLLMConfig(
             model_name="Qwen/Qwen3-0.6B",
@@ -99,6 +125,7 @@ class TestLLMs:
         response_msg = llm.generate(messages=messages, config=self.llm_gen_config())
         assert check_response(response_msg) is True
 
+    # @pytest.mark.skip(reason="skip sglang llm")
     def test_sglang_gen(self, input_prompt):
         llm_gen_config = LLMGenerateConfig(
             max_n_tokens=100, temperature=0.7, logprobs=True, seed=42
@@ -109,6 +136,7 @@ class TestLLMs:
         msg = llm.generate(messages=msg, config=llm_gen_config)
         assert check_response(msg) is True
 
+    # @pytest.mark.skip(reason="skip ollama llm")
     def test_ollama_gen(self, input_prompt):
         llm_gen_config = LLMGenerateConfig(
             max_n_tokens=100, temperature=0.7, logprobs=True, seed=42
